46c97a3f5ecfee2704b2f54c4f04d6439db4475c
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-16 21:03:37 +0000

    Added 'it-do' test statements to record_spec file

--- record.rb
+++ record.rb
@@ -1,30 +1,3 @@
 class Record
-    #2 arrays, voter and politician, 4methods, CRUD
-    #aray voter - push, += or -= everytime you add or subtr a voter
-    # create recors.create -> script in main.rb
-    
-    it "can create a voters array" do
-    end
-    
-    it "can create a politician's array" do
-    end
-    
-    it "can list voters" do
-    end
-    
-    it "can list politicians" do
-    end
-    
-    it "can update a voter" do
-    end
-    
-    it "can update a politician" do
-    end
-    
-    it "can delete voters" do
-    end
-    
-    it "can delete politicians" do
-    end
-       
+   
 end
--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,7 +1,33 @@
 require './record.rb'
 
 describe Record do
-    it "creates an array of persons" do
+     #2 arrays, voter and politician, 4methods, CRUD
+    #aray voter - push, += or -= everytime you add or subtr a voter
+    # create recors.create -> script in main.rb
+    # main menu does not get tested. functions only called.
+    
+    it "can create a voters array" do
     end
-    it ""
+    
+    it "can create a politician's array" do
+    end
+    
+    it "can list voters" do
+    end
+    
+    it "can list politicians" do
+    end
+    
+    it "can update a voter" do
+    end
+    
+    it "can update a politician" do
+    end
+    
+    it "can delete voters" do
+    end
+    
+    it "can delete politicians" do
+    end
+       
 end
