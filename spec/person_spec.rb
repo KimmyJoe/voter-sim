64808e1d0e4a11ae1ce2e30f2afec376f7df98bb
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-16 00:49:56 +0000

    Test for initializing a person is passing

---
+++ spec/person_spec.rb
@@ -0,0 +1,11 @@
+require './person.rb'
+
+describe Person do
+    it "creates a person with a name" do
+        person = Person.new("John Doe")
+        expect(person).to be_a(Person)
+    end
+    
+    it "can report its name"
+
+end
