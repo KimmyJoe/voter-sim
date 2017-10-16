25d4a90406730d3616c2e31bc586d9ea311a4a6e
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-16 21:12:45 +0000

    Comments added & Hints

--- record.rb
+++ record.rb
@@ -1,3 +1,30 @@
 class Record
-   
+    
+    # @voters = []
+   #def create_voter()
+   #end
+   #
+   #def list_all_voters
 end
+
+### Main Menu
+#
+#1. Create an instance of records
+#voter_records = Records.new
+#
+#2. Ask the user for input
+#puts "What do you want to do?"
+#puts "Options: CRUD"
+#
+#user_input = gets.chomp
+#
+#case user_input
+#   #handle create
+#when "C"
+#   records.create_voter
+#   #handle list 
+#when "L"
+#   records.list_all_voters
+#   
+#   
+#   ^ require these...
