52bb4165369c1c6371b65b28736c6e2bce27be74
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-16 00:54:30 +0000

    Test for reading name passed

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,6 +6,9 @@ describe Person do
         expect(person).to be_a(Person)
     end
     
-    it "can report its name"
+    it "can report its name" do
+        person = Person.new("John Doe")
+        expect(person.name).to eq("John Doe")
+    end
 
 end
