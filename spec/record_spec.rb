86682fd7e6f021d2dca1b8bb687e72a3460fcb32
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-20 02:21:52 +0000

    Added more test examples to Records spec to satisfy requirements

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,63 +1,13 @@
 require './record.rb'
 
 describe Record do
-     #2 arrays, voter and politician, 4methods, CRUD
-    #aray voter - push, += or -= everytime you add or subtr a voter
-    # create recors.create -> script in main.rb
-    # main menu does not get tested. functions only called.
-    # 
-    # it is created
-    # it can create a voter record
-    # it can create a politician record
-    # it can list all voters
-    # it can list all politicians
-    # it can update a voters
-    # it can update a politicians
-    # it can delete a voters
-    # it can delete a politician
-    # 
-    # 
-    # ^^^^
-    # 
-    # test instantiation of records
-    # test creation of voter and politician
-    # test voters and politicians are stored in arrays, test we can use them
-    # voters and politicians should be updatable
-    # voters and politicians should be removable
-    # 
-    # ^^^^
-    # 
-    # 
-    # 
-    # 
-    # 
-    # 
-    # 
-    
-    it "can create a voters array" do
-        voters = ["John", "Johnny", "Jim", "Jay"]
-        
-    end
-    
-    it "can create a politician's array" do
-    end
-    
-    it "can list voters" do
-    end
-    
-    it "can list politicians" do
-    end
-    
-    it "can update a voter" do
-    end
-    
-    it "can update a politician" do
-    end
-    
-    it "can delete voters" do
-    end
-    
-    it "can delete politicians" do
-    end
-       
+    it "can create a voters array"
+    it "can create a politician's array"
+    it "can list all voters and politicians on record"
+    it "can search for a voter by name"
+    it "can search for a politician by name"
+    it "can update a voter"
+    it "can update a politician"
+    it "can delete voters"
+    it "can delete politicians"       
 end
