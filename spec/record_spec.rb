31ce4d6386f5a2af45eb085ee915dd90807c5074
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-20 02:29:38 +0000

    Updated examples for Records

--- spec/record_spec.rb
+++ spec/record_spec.rb
@@ -1,8 +1,8 @@
 require './record.rb'
 
 describe Record do
-    it "can create a voters array"
-    it "can create a politician's array"
+    it "can add a voter to a voters array"
+    it "can add a politician to a politicians arra"
     it "can list all voters and politicians on record"
     it "can search for a voter by name"
     it "can search for a politician by name"
