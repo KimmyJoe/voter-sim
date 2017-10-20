dd7296b9dfecc3f5ecc137117209e6a7715e7cac
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-20 02:57:25 +0000

    Test to add a politician to a politicians array passed.

--- records.rb
+++ records.rb
@@ -1,8 +1,9 @@
 class Records 
     attr_accessor :voters
+    attr_accessor :politicians
     
     def initialize 
         @voters = []
+        @politicians = []
     end
-
 end
--- spec/records_spec.rb
+++ spec/records_spec.rb
@@ -1,5 +1,6 @@
 require './records.rb'
 require './voter.rb'
+require './politician.rb'
 
 describe Records do
     it "can add a voter to a voters array"  do
@@ -12,7 +13,16 @@ describe Records do
         expect(records.voters.last.political_affiliation).to eq("Liberal")
     end
     
-    it "can add a politician to a politicians array"
+    it "can add a politician to a politicians array" do
+        politician = Politician.new("Jane Doe", "Republican")
+        
+        records = Records.new
+        records.politicians.push(politician)
+        
+        expect(records.politicians.last.name).to eq("Jane Doe")
+        expect(records.politicians.last.party_affiliation).to eq("Republican")
+    end
+    
     it "can list all voters and politicians on record"
     it "can search for a voter by name"
     it "can search for a politician by name"
