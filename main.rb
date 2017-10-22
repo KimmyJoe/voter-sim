3127dadd37431ab7ff4abf92cfc295d885255597
Kimberley Joe <kimmycjoe@gmail.com>
2017-10-22 18:30:45 +0000

    Create Operation refactored to creat and store new charater with attributes.

--- main.rb
+++ main.rb
@@ -1,36 +1,59 @@
 require './records.rb'
 
-# Variables for main menu
-records = Records.new
+class Main
+    def initialize
+        @records = Records.new
+    end
 
-# Methods for main menu
-def display_welcome
-    puts "Welcome! This Voter-Simulation will allow you to create voters and politicians."
-    puts "Once you have created voters and politicians, you will be given the option to display all created characters."
-    puts "You will also have the option to modify or delete each voter or politician after searching for them by name." 
-end 
-
-def create_operation
-    puts "What would you like to create?"
-    puts "(P)olitician or (V)oter"
-    user_input = gets.chomp.downcase.strip
-end
-
-def main_menu
-    puts "What would you like to create?"
-    puts "(C)reate, (L)ist, (U)pdate, or (D)elete"
-    desired_operation = gets.chomp.downcase.strip
+    # Methods for main menu
+    def display_welcome
+        puts "Welcome! This Voter-Simulation will allow you to create voters and politicians."
+        puts "Once you have created voters and politicians, you will be given the option to display all created characters."
+        puts "You will also have the option to modify or delete each voter or politician after searching for them by name." 
+    end 
     
-    case desired_operation
-        when "c"
-            create_operation
-        else
-            puts "Oops! We don't support that"
-    end       
+    def display_menu
+        puts "What would you like to create?"
+        puts "(C)reate, (L)ist, (U)pdate, or (D)elete"
+        desired_operation = gets.chomp.downcase.strip
+
+        case desired_operation
+            when "c"
+                create_operation
+
+            else
+                puts "Oops! We don't support that"
+        end       
+    end
+
+    def create_operation
+        puts "What would you like to create?"
+        puts "(P)olitician or (V)oter"
+        user_input = gets.chomp.downcase.strip
+
+        case user_input
+            when "p"
+                puts "Name?"
+                name = gets.chomp.downcase.strip
+                puts "Party?"
+                puts "(D)emocrat or (R)epublican"
+                affilitaion = gets.chomp.downcase.strip
+                @records.create_politician(name, affilitaion)
+            when "v"
+                puts "Name?"
+                name = gets.chomp.downcase.strip
+                puts "Politics?"
+                puts "(L)iberal, (C)onservative, (T)ea Party, (S)ocialist, or (N)eutral"
+                affilitaion = gets.chomp.downcase.strip
+                @records.create_voter(name, affilitaion)
+            else
+                puts "Oops! We don't support that"  
+        end
+    end
 end
 
-# Main menu
-display_welcome
+main = Main.new
+main.display_welcome
+main.display_menu
 
-main_menu
 
